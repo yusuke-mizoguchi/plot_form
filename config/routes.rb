8b9b39e9b07623c97334759b17c72bd64070d720
yusuke-mizoguchi <zunmokou@gmail.com>
2022-02-22 22:34:21 +0900

    user、sessionコントローラーの作成。webpackerのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
+  resources :novels do
+    resources :review
+  end
 end
