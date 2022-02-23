520054337caf4c8f5857a52ef516a682b0c601be
yusuke-mizoguchi <zunmokou@gmail.com>
2022-02-23 22:01:09 +0900

    reviewコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
   resources :novels do
-    resources :review
-    resources :character
+    resources :review, only: %i[create edit update destroy]
+    resources :character, only: %i[create edit update destroy]
   end
 end
