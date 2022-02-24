c821069b8ac16f70684406376538536419ec416f
yusuke-mizoguchi <zunmokou@gmail.com>
2022-02-24 22:02:57 +0900

    Add:ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  get 'rule', to: 'static_pages#rule'
+  get 'privacy', to: 'static_pages#privacy'
   
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
