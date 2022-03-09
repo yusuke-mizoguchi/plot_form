b75b3b5ed2cdd1d6dc296b24a117ebd68dd840a3
yusuke-mizoguchi <zunmokou@gmail.com>
2022-03-09 19:59:42 +0900

    Add:バリデーション抜けを補修

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :novel
 
-  validates :good_point, presence: true
+  validates :good_point, presence: true, length: { maximum: 1500 }
+  validates :bad_point, length: { maximum: 1500 }
 end
